84a0f25e77f92cc9067576ecc66f5666029f0b88
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-01-31 13:44:23 +0700

    tambah enum jenis barang

---
+++ src/main/java/com/rizki/mufrizal/belajar/thymeleaf/model/JenisBarang.java
@@ -0,0 +1,15 @@
+package com.rizki.mufrizal.belajar.thymeleaf.model;
+
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Since Jan 31, 2016
+ * @Time 12:41:39 PM
+ * @Encoding UTF-8
+ * @Project Belajar-Thymeleaf
+ * @Package com.rizki.mufrizal.belajar.thymeleaf.model
+ * 
+ */
+public enum JenisBarang {
+    cair, gas, padat
+}
